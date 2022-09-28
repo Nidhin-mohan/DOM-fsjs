643431988c25bdac4bcaf5c75242937931ad5507
Nidhin-mohan <nidhinmohannidhin>
2022-09-28 20:52:32 +0530

    Initial commit

---
+++ Answers/Readmeans.md
@@ -0,0 +1,10 @@
+# answers Of Dom Assingnments
+
+# 1. Dev.io
+
+``
+document.querySelector(".side-bar .crayons-card .crayons-subtitle-2  ").innerHTML = "Nidhin"
+
+``
+
+![image]()
---
+++ Answers/script.js
@@ -0,0 +1,119 @@
+
+// 1.
+
+// document.querySelector(".side-bar .crayons-card .crayons-subtitle-2  ").innerHTML = "Nidhin"
+
+// document.querySelector(".side-bar .crayons-card .color-base-70   ").innerHTML =
+//   "I am a fullstack Web Developer from Kerala";
+
+
+ 2
+
+
+arr = document.querySelector(".as-imagegrid .as-imagegrid-item-title  ");
+
+
+
+let arr = document.querySelectorAll(".as-imagegrid .as-imagegrid-item-title");
+let newarr = [...arr].map((i) => {
+  return i.textContent.replace(" Support", "");
+});
+
+
+
+3.
+
+
+
+let nav = document.querySelector(".accordion-homepage")
+
+let sec = document.createElement("section");
+sec.className = "parent";
+
+let h3 = document.createElement("h3");
+
+h3.innerHTML = "My FAQ ";
+
+sec.appendChild(h3);
+nav.appendChild(sec);
+
+
+
+4.
+document.querySelector(" .customer-support a ").inner
+
+
+5.
+
+document.querySelector(
+  " .mytabs    .diwali-deals-product-sale-pro .diwali-deals-product-sale-btn"
+);
+
+6.
+
+
+ let serch = document.querySelector(
+   ".searchinput-wrapper___3YrvF .searchinput___19uW0"
+ );
+
+serch.addEventListener("mouseover",function(e){
+    e.target.style.backgroundColor = "red"})
+
+
+
+
+
+
+
+
+
+ 8.
+  const div = document.querySelector("#SIvCob");
+  div.remove();
+
+
+  9. 
+
+  document.querySelector(
+    ".content-width-extra-large .display-heading-1"
+  ).style.fontFamily = "monospace";
+
+   document.querySelector(
+    ".content-width-extra-large .display-heading-1"
+  ).style.color = "#A7331C";
+
+
+  10.
+
+
+    document.querySelector(".btn-cta-big .login-btn-text")
+     .addEventListener("mouseover", () => {
+      .style.backgroundColor = "red"
+     });
+
+
+     let btn = document.querySelector(".btn-cta-big .login-btn-text");
+     btn.addEventListener("mouseover", () => btn.style.backgroundColor = "red")
+  
+
+     11.
+
+    let logo = document.querySelector(" .header .wrapper .logo .icon")
+     logo.style.backgroundImage = "url('https://ineuron.ai/images/ineuron-logo.png')"; 
+
+     12.
+
+     let btns = document.querySelector(".js-repos-container .f4 .btn")
+     btns.style.backgroundColor = "blue";
+
+
+     13.
+
+     document.querySelector(".fl-heading .fl-heading-text").innerHTML ="JS bootcamp"
+
+     14.
+
+document.querySelector(".HotDealsAll__Heading__2fIbe").style.fontSize = "80px"
+
+
+     
